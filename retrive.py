4609350c0a064184ed58c63f5e7b1112ffcc05ca
Suriya Parithy <95739525+parithysuriya@users.noreply.github.com>
2022-08-29 18:26:32 +0530

    Update retrive.py

--- retrive.py
+++ retrive.py
@@ -12,8 +12,8 @@ import io
 mydb = mysql.connector.connect(
 	host="localhost",
 	user="root",
-	password="00000000",
-	database="sedatabase" # Name of the database
+	password="12345678",
+	database="database" # Name of the database
 )
 
 # Create a cursor object
